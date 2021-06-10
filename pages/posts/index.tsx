b72fe202bfeec10a938ae06d74a87a60ef74b4f1
Владимир <zelleaz@mail.ru>
2021-06-10 13:05:17 +0500

    refactor and finally done

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next'
-import React, { useEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import { getPosts } from '../../api/api'
+import { Info } from '../../components/Info/Info'
 import { Navbar } from '../../components/Navbar/Navbar'
 import { PostsList } from '../../components/Posts/PostsList'
 import { useAppDispatch, useAppSelector } from '../../hooks/redux'
@@ -9,26 +10,31 @@ import { IPost } from '../../store/types/jsonPlaceholder'
 
 const Posts: NextPage<{propsPosts: IPost[]}> = ({ propsPosts }) => {
   const { posts } = useAppSelector(state => state.posts)
+  const { currentUser } = useAppSelector(state => state.users)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     dispatch(loadPostsCreator(propsPosts))
   }, [])
 
-  let preparedPosts: IPost[] = propsPosts.slice(-20)
+  let preparedPosts: IPost[] = []
   
-  useEffect(() => {
+  if (currentUser.id !== 0) {
+    preparedPosts = posts.filter(post => post.userId === currentUser.id)
+  } else {
     preparedPosts = posts.slice(-20)
-  }, [posts])
+  }
+
+  
 
   return (
     <div className="container">
       <Navbar />
       <div className='page'>
         <h1 className="page__title">Посты</h1>
-
         <PostsList posts={preparedPosts} />
       </div>
+      {currentUser.id !== 0 && <Info />}
     </div>
   )
 }
